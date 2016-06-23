744b265d37dd4835b5c81494daf79b8ed074b3ca
Carson Levine <carsonglevine@me.com>
2016-06-23 01:25:11 +0000

    updated application controller with params and array

--- application_controller.rb
+++ application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/new_project' do
+    erb :new_project
+  end
+
+  post '/new_project' do
+    SinatraProject.new(params["project_name"], params["creators"], params["link"])
+    @projects_array.push([params["project_name"], params["creators"], params["link"]])
+    erb :index
+  end
+
 end
--- models/model.rb
+++ models/model.rb
@@ -1,9 +1,20 @@
 class SinatraProject
+
   attr_accessor :project_name, :creators, :link, :cover_image
+  @count = 0
+  @projects_array => {[]}
+
   def initialize(project_name, creators, link, cover_image)
-    @project_name = project_name
-    @creators = creators
-    @link = link
-    @cover_image = cover_image
+    @count = @count + 1
+
   end
+
+  def self.all
+    @projects_array.inspect
+  end
+
+  def self.count
+    @count
+  end
+
 end
